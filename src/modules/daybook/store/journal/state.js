1ed9bcaa15e2adeab306c2400111ff62520d0051
tufinhon <40000609+tufinhon@users.noreply.github.com>
2021-11-22 23:11:04 -0800

    Primer commit

---
+++ src/modules/daybook/store/journal/state.js
@@ -0,0 +1,24 @@
+
+export default () => ({
+  isLoading: true,
+  entries: [
+    {
+      id: new Date().getTime(), //12223453253245
+      date: new Date().toDateString(), // sat 23, julio
+      text: 'Ad amet labore in sint dolore esse ullamco velit labore. Est cupidatat est excepteur eu labore culpa dolor cupidatat adipisicing cillum dolore est enim nostrud. Sunt occaecat incididunt do deserunt veniam aliqua nulla veniam nostrud cillum ad labore aute. Reprehenderit fugiat aliquip magna officia enim. Aliqua pariatur officia magna excepteur commodo velit do aliqua pariatur magna officia.',
+      picture: null, // https://
+    },
+    {
+      id: new Date().getTime()+1000, //12223453253245
+      date: new Date().toDateString(), // sat 23, julio
+      text: 'Consectetur aliquip do labore qui ullamco dolor cillum cillum ullamco excepteur eu sunt excepteur. Sunt anim sint culpa non. Incididunt eiusmod ullamco laboris consequat aliquip nulla enim consequat commodo amet irure. Sit cupidatat esse sint do. Eu id occaecat labore incididunt dolor.',
+      picture: null, // https://
+    },
+    {
+      id: new Date().getTime()+2000, //12223453253245
+      date: new Date().toDateString(), // sat 23, julio
+      text: 'Quis aute consequat quis labore id officia ut deserunt mollit magna laborum ipsum est nulla. Lorem ea reprehenderit elit anim est consequat veniam do in sit sint nulla fugiat magna. Sint ut eiusmod anim mollit. Est et commodo laboris adipisicing est tempor dolore commodo consequat sint voluptate esse. Nostrud amet incididunt velit ullamco. Nisi deserunt consectetur nulla amet minim eu sint cupidatat fugiat proident veniam est esse dolore.',
+      picture: null, // https://
+    },
+  ]
+})
